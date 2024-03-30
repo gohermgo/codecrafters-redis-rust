88a267fb382852ca860e6d2270b9c0bbb07b40a9
gohermgo <gohnherman@gmail.com>
2024-03-30 10:28:53 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
-use std::net::TcpListener;
+use std::{fmt, net::TcpListener};
+
+pub enum RESPData<'a> {
+    Str(&'a str),
+}
+
+impl fmt::Display for RESPData<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            RESPData::Str(payload) => f.write_fmt(format_args!("+{}\r\n", payload)),
+        }
+    }
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
